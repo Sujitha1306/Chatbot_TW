ed37a20ed3a4d11ec317d6b0748940fbf8cec508
Sujitha1306 <sujitha@example.com>
2026-06-30 16:44:40 +0530

    chore: remove test scripts from root directory

--- test_memory_scope.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-import asyncio
-sys.path.append(os.getcwd())
-from backend.app.core.sql_pipeline import SQLGenerationPipeline
-pipeline = SQLGenerationPipeline()
-res = pipeline.resolve_memory_scope("tell me my name", "", False)
-print("Memory scope result:", res)
